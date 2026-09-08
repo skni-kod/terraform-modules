0940b2a911497e9bf29c4ef16f86e1b14788239d
matzajdel <mateusz.zajdel111@gmail.com>
2026-09-08 17:53:29 +0200

    Add template for creating Proxmox VM

---
+++ proxmox-vm/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    proxmox = {
+        source = "bpg/proxmox"
+        version = "~> 0.66"
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "this" {
+    name = var.name
+    node_name = var.target_node
+    vm_id = var.vmid
+
+    tags = var.tags
+
+    agent {
+        enabled = false
+    }
+
+    cpu {
+        cores = var.cores
+        type = "host"
+    }
+
+    memory {
+        dedicated = var.memory
+    }
+
+    disk {
+        datastore_id = var.datastore_id
+        interface = "scsi0"
+        size = var.disk_size
+        file_format = "raw"
+    }
+
+    clone {
+        vm_id = var.cloned_vm_id
+        full = true
+    }
+
+    network_device {
+        bridge = var.network_bridge
+        model = "virtio"
+    }
+
+    initialization {
+        ip_config {
+            ipv4 {
+                address = var.ip_address
+                gateway = var.gateway
+            }
+        }
+
+        user_account {
+            username = var.ci_user
+            keys = [var.ssh_public_key]
+        }
+    }
+
+    # Added for readability, below options are set by default 
+    scsi_hardware = "virtio-scsi-pci"
+    boot_order = ["scsi0"]
+    serial_device {}
+    vga {
+        type = "serial0"
+    }
+
+}
